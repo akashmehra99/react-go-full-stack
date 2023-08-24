7a5250044a5f3eefacb6033cad985c56d28d83ef
Akash Mehra <akash.mehra99@gmail.com>
2023-08-24 12:20:30 +0530

    Code changes for cokkie not setting up in chromium browser and adding admin route guard

--- movies-fe/src/components/manage-catalogue/manage-catalogue.component.jsx
+++ movies-fe/src/components/manage-catalogue/manage-catalogue.component.jsx
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useOutletContext } from 'react-router-dom';
 
 export const ManageCatalogue = () => {
+    const [movies, setMovies] = useState([]);
+    const { jwtToken } = useOutletContext();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (jwtToken === '') {
+            navigate('/login');
+            return;
+        }
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Authorization', 'Bearer ' + jwtToken);
+        const requestOptions = {
+            method: 'GET',
+            headers: headers,
+        };
+        fetch('/admin/movies', requestOptions)
+            .then((res) => res.json())
+            .then((data) => {
+                setMovies(data);
+            })
+            .catch((err) => console.error(err));
+    }, [jwtToken, navigate]);
+
     return (
         <div>
             <h2>Manage Catalogue</h2>
             <hr />
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Movie</th>
+                        <th>Release Date</th>
+                        <th>Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map((movie) => (
+                        <tr key={movie.id}>
+                            <td>
+                                <Link to={`/admin/movies/${movie.id}`}>
+                                    {movie.title}
+                                </Link>
+                            </td>
+                            <td>{movie.release_date}</td>
+                            <td>{movie.mpaa_rating}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
-}
+    );
+};
